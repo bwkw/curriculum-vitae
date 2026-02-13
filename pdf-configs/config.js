0cea0cb0d82d374555a8fa2f1507a27b9193b13d
bwkw <na.shota.1139@gmail.com>
2026-02-13 09:46:03 +0900

    feat: デフォルトレイアウトと PDF 設定を更新
    - HTML レイアウトを改善し、ダウンロードリンクを追加
    - PDF 生成時のオプションを追加し、mermaid のテーマを設定

--- pdf-configs/config.js
+++ pdf-configs/config.js
@@ -5,6 +5,12 @@ module.exports = {
     headerIds: false,
     smartypants: true,
   },
+  launch_options: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  mermaid_options: {
+    theme: "default",
+  },
   pdf_options: {
     format: "A4",
     margin: "30mm 20mm",
